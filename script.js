53af65edf760260a02c9a7a4b7870f190d77a5e1
gugagogitidze <gugsonsss31@gmail.com>
2024-10-18 06:09:20 +0400

    java
    javassss momgonssss

---
+++ script.js
@@ -0,0 +1,17 @@
+let Name = "guga";
+console.log(Name);
+
+let Lastname = "gogitidze";
+console.log(Lastname);
+
+let Age = 18;
+console.log(Age);
+
+let agechecked = true;
+console.info("%cadult", "color: yellow");
+
+let email = "gugsonsss31@gmail.com";
+console.log(email);
+
+let number = 52222044;
+console.info("%c522220488", "color:red");
